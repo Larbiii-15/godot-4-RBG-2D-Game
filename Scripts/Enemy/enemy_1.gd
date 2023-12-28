213ac1872bdfe89105a3e063e15f2be5789dcf30
Larbiii-12 <mohamedino.aloui@gmail.com>
2023-12-28 12:51:09 +0100

    Coder les mvts & Changer la direction de notre ennemie aleat

--- Scripts/Enemy/enemy_1.gd
+++ Scripts/Enemy/enemy_1.gd
@@ -1,1 +1,9 @@
 extends entity_movement
+
+func _ready():
+	random_direction() # on appelle la fonction random ici de la script entity.gd
+# Timer : Nouveau node --> en fin de la décomptage, on va actionner on va appeler notre fonction random direction
+
+func _on_timer_timeout():
+	random_direction()
+	$Timer.start()    # On va relancer le time start qui va créer une boucle qui va donc à chaque fois changer la direction de notre personnage
--- Scripts/Entity/entity.gd
+++ Scripts/Entity/entity.gd
@@ -23,28 +23,48 @@ func _physics_process(delta):
 			move_right(delta)
 	
 	move_and_slide()
-			
+	
+func random_direction():
+	dir = randi() % 4 + 1 # fonction qui permette de générer une suite de nombrs entier entre 0 et 4 // le plus 1 permettre de soustraire le zéro.
+	print(dir)
+	random_mouvement()
+	
+func random_mouvement():
+	match dir:  # match statement
+		1:
+			current_states = enemy_states.MOVELEFT
+		2:
+			current_states = enemy_states.MOVERIGHT
+		3:
+			current_states = enemy_states.MOVEUP
+		4:
+			current_states = enemy_states.MOVEDOWN
+
 func move_down(delta):
 	Vector2.DOWN # pour que notre ennemie bouge sans le contrôler
 	velocity.y += speed * delta  # bouge sur l'axe vertical Y et multipler par delta pour contrôler la vitesse de notre ennemie
 	velocity.y = clamp(-speed, 15 , speed)  # pour mettre une limite à la vitesse pour qu'il ne dépasse pas 
+	velocity.x = 0 # sinon l'ennemi reste se déplace d'une sorte diagonale bizarre.
 	$anim.play("move_down")
 func move_up(delta):
-	Vector2.UP # pour que notre ennemie bouge sans le contrôler
-	velocity.y -= speed * delta  # bouge sur l'axe vertical Y et multipler par delta pour contrôler la vitesse de notre ennemie
-	velocity.y = clamp(speed, 15 , -speed)  # pour mettre une limite à la vitesse pour qu'il ne dépasse pas 
+	Vector2.UP 
+	velocity.y -= speed * delta  
+	velocity.y = clamp(speed, 15 , -25)  
+	velocity.x = 0
 	$anim.play("move_up")
 
 func move_left(delta):
-	Vector2.LEFT # pour que notre ennemie bouge sans le contrôler
-	velocity.x -= speed * delta  # bouge sur l'axe vertical Y et multipler par delta pour contrôler la vitesse de notre ennemie
-	velocity.x = clamp(speed, 15 , -speed)  # pour mettre une limite à la vitesse pour qu'il ne dépasse pas 
+	Vector2.LEFT 
+	velocity.x -= speed * delta  
+	velocity.x = clamp(speed, 15 , -25)  
+	velocity.y = 0
 	$anim.play("move_left")
 
 func move_right(delta):
-	Vector2.RIGHT # pour que notre ennemie bouge sans le contrôler
-	velocity.x += speed * delta  # bouge sur l'axe vertical Y et multipler par delta pour contrôler la vitesse de notre ennemie
-	velocity.x = clamp(-speed, 15 , speed)  # pour mettre une limite à la vitesse pour qu'il ne dépasse pas 
+	Vector2.RIGHT 
+	velocity.x += speed * delta 
+	velocity.x = clamp(-speed, 15 , speed)  
+	velocity.y = 0
 	$anim.play("move_right")
 
 
